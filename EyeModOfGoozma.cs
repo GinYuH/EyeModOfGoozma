2921b23db3fcb972bc14c9b6382fd20a1025fef4
GinYuH <72181395+GinYuH@users.noreply.github.com>
2023-10-08 03:53:56 -0700

    Add files via upload

---
+++ EyeModOfGoozma.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace Goozmeye
+{
+	public class Goozmeye : Mod
+	{
+	}
+}
---
+++ GoozmaEyeNPC.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace EyeModOfGoozma
+{
+    public class GoozmaEyeNPC : GlobalNPC
+    {
+        public override bool InstancePerEntity => true;
+
+        public override void OnKill(NPC npc)
+        {
+            if (npc.type == ModContent.NPCType<CalamityHunt.Content.Bosses.Goozma.Goozma>())
+            {
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                    Vector2 speed = new Vector2(0, 4);
+                    Projectile.NewProjectile(npc.GetSource_FromAI(), npc.Center, speed, ModContent.ProjectileType<GoozmaEye>(), 0, 0f, Main.myPlayer);
+            }
+            }
+        }
+    }
+}
